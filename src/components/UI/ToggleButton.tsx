f515047e5dfb9b92f3529dd682a25ee316e28180
Marcin Sobieraj <marsobpro@gmail.com>
2024-11-28 16:30:38 +0100

    Added error handling in main.tsx

--- src/components/UI/ToggleButton.tsx
+++ src/components/UI/ToggleButton.tsx
@@ -17,7 +17,7 @@ export const ToggleButton: React.FC<ToggleButtonProps> = ({
     <button
       onClick={onToggle}
       disabled={disabled}
-      className="text-white text-sm transition-colors hover:bg-gray-800 disabled:bg-black/75 bg-black rounded px-3 py-1 hover:opacity-70 duration-150 cursor-pointer"
+      className="text-white text-sm transition-colors hover:bg-gray-800 disabled:bg-black/75 bg-black rounded px-3 py-1 hover:opacity-70 duration-150 cursor-pointer disabled:opacity-40"
     >
       {isToggled ? onLabel : offLabel}
     </button>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Failed to find the root element");
+}
+
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
